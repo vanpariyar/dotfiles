61d643662d3198475b01d33d25404039f8b8016f
Ronak J Vanpariya <26689210+vanpariyar@users.noreply.github.com>
2024-03-24 16:49:49 +0530

    FIX: git

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -25,7 +25,7 @@ source ~/.zprofile
 #git clone git@github.com:vanpariyar/dotfiles.git ~/.dotfiles
 
 # ...or use HTTPS and switch remotes later.
-git clone https://github.com/vanpariyar/dotfiles.git ~/.dotfiles/macos
+git clone https://github.com/vanpariyar/dotfiles.git ~/.dotfiles
 
 # There are better and less manual ways to do this;
 # investigate install scripts and bootstrapping tools.
